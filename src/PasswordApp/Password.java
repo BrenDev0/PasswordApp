4ad0c4194d54a09bcea42be52c7a46ae2da765e9
Brendan Lahey <lahey1991@gmail.com>
2024-09-21 18:23:28 -0500

    WIP: user input bug
    corrected error after user input

--- src/PasswordApp/Password.java
+++ src/PasswordApp/Password.java
@@ -12,20 +12,65 @@ public class Password {
 		System.out.print("Enter the site where this password is needed: ");
 		Scanner thesite = new Scanner(System.in);
 		this.site = thesite.nextLine();
-		thesite.close();
+		
 		
 		// Ask if a randomly generated password is needed, else receive password.
 		System.out.print("Password Options.\n1 - Enter password manualy.\n2 - Generate a random password.\nEnter code here: ");
+		Scanner  in = new Scanner(System.in);
+		int passwordOption = in.nextInt();
+		
+		switch(passwordOption) {
+			case 1:
+				enterPassword();
+				break;
+			case 2:
+				generateRandomPassword();
+				break;
+			default:
+				break;
+		}
+		
 		
 		// submit data to database 
 	}
 	
+	//
+	
 	// Generate a random Password
+	private String generateRandomPassword() {
+		return "";
+	}
 	
 	// Submit password to database
 	
 	// Get password 
 	
-	// Set new password 
+	// enter a  password
+	private void enterPassword() {
+		// Get password from user 
+		String enteredPassword;
+		System.out.print("Enter a password: ");
+		Scanner firstInput = new Scanner(System.in);
+		enteredPassword = firstInput.nextLine();
+		
+		
+		// Verify the password before setting
+		System.out.print("Enter password again: ");
+		Scanner verifyPassword = new Scanner(System.in);
+		String verifiedPassword = verifyPassword.nextLine();
+		
+		if (verifiedPassword.equals(enteredPassword)) {
+			this.password = enteredPassword;
+			System.out.println("Password accepted!");
+			verifyPassword.close();
+		} else {
+			System.out.println("Passwords did not match try again...");
+			enterPassword();
+		}
+			
+	}
+	
+	
+	// 
 
 }
