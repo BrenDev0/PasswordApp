8e61c596a2d64c0cbd1c5975c9e535292a49cc85
Brendan Lahey <lahey1991@gmail.com>
2024-09-22 10:37:04 -0500

    feat: random password generator function

--- src/PasswordApp/Password.java
+++ src/PasswordApp/Password.java
@@ -1,5 +1,7 @@
 package PasswordApp;
 import java.util.Scanner;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
 
 public class Password {
 	private String password;
@@ -24,7 +26,7 @@ public class Password {
 				enterPassword();
 				break;
 			case 2:
-				generateRandomPassword();
+				System.out.print(generateRandomPassword());
 				break;
 			default:
 				break;
@@ -38,7 +40,53 @@ public class Password {
 	
 	// Generate a random Password
 	private String generateRandomPassword() {
-		return "";
+		System.out.print("How many characters must the password contain(min 8 characters): ");
+		Scanner lengthIn = new Scanner(System.in);
+		int passwordLength = lengthIn.nextInt();
+		char[] randomPassword = new char[passwordLength];
+		String uppercase = "ABCDEFGHIJKLMNOPQRZTUVWXYZ";
+		String lowercase = "abcdefghijklmnopqrstuvwxyz";
+		String nums = "0123456789";
+		String specialChars = "!@#$%^&*";
+		String passwordSet = uppercase + lowercase + nums + specialChars;
+		// fill the array with random characters
+		for(int i = 0; i < passwordLength; i++) {
+			int randomChar = (int) (Math.random() * passwordSet.length());
+			randomPassword[i] = passwordSet.charAt(randomChar);
+		}
+		
+		// password requirements
+		Pattern uppercaseChar = Pattern.compile("[A-Z]");
+		Pattern number = Pattern.compile("[0-9]");
+		Pattern symbols = Pattern.compile("[^a-zA-Z0-9]");
+		
+		
+		// check to see if all requirements are present, if not insert them
+		Matcher uppercaseRe = uppercaseChar.matcher(new String(randomPassword));
+		if (!uppercaseRe.find()) {
+			int randomIndex = (int) (Math.random() * passwordLength);
+			int randomUppercaseChar = (int) (Math.random() * uppercase.length());
+			
+			randomPassword[randomIndex] = uppercase.charAt(randomUppercaseChar);
+		}
+		
+		Matcher numberRe = number.matcher(new String(randomPassword));
+		if (!numberRe.find()) {
+			int randomIndex = (int) (Math.random() * passwordLength);
+			int randomNum = (int) (Math.random() * nums.length());
+			
+			randomPassword[randomIndex] = nums.charAt(randomNum);
+		}
+		
+		Matcher symbolsRe = symbols.matcher(new String(randomPassword));
+		if (!symbolsRe.find()) {
+			int randomIndex = (int) (Math.random() * passwordLength);
+			int randomSymbol = (int) (Math.random() * specialChars.length());
+			
+			randomPassword[randomIndex] = specialChars.charAt(randomSymbol);
+		}
+		//return password to user 
+		return "Password For " + this.site + " is: " + new String(randomPassword);
 	}
 	
 	// Submit password to database
